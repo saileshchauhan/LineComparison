7c43119d6d580f17dcc3f2be64398eac9eb4b918
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-08 08:38:49 +0530

    [sailesh] Add: Distance Calculation

---
+++ Distance.cs
@@ -0,0 +1,28 @@
+﻿namespace Line_Comparison
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    
+    public class Distance
+    {
+
+        public double distance;
+        public int x2, y2, x1, y1;
+        public Distance(int x1,int y1,int x2,int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        public double GetDistance()
+        {
+            
+            distance = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+            return distance;
+        }   
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+ public class Program
+    {
+        public double lengthOne, lengthTwo;
+        static void Main(string[] args)
+        { 
+            Distance lineOne = new Distance(1,1,2,2);
+            double lengthOne = lineOne.GetDistance();
+            Console.WriteLine("LengthOne"+lengthOne);
+            Distance lineTwo = new Distance(1, 1, 3, 3);
+            double lengthTwo = lineTwo.GetDistance();
+            Console.WriteLine("LengthTwo  " + lengthTwo);
+
+            //Equals Method
+            EqualsMethod equalCheck = new EqualsMethod(lengthOne,lengthTwo);
+            equalCheck.FunctionEqualsMethod();
+
+            //CompareTo Method
+            CompareToMethod compareCheck = new CompareToMethod(lengthOne,lengthTwo);
+            compareCheck.FuncCompareToMethod();
+
+        }
+        
+    }
+}
