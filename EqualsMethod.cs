287b96cd7f1c2c149234edac776672f982bd2614
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-08 08:42:52 +0530

    [sailesh] Add: Equals Method

---
+++ EqualsMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Line_Comparison
+{
+    class EqualsMethod
+    {
+        public double lengthOne, lengthTwo;
+
+        public EqualsMethod(double lengthOne,double lengthTwo)
+        {
+            this.lengthOne = lengthOne;
+            this.lengthTwo = lengthTwo;
+        }
+        public void FunctionEqualsMethod()
+        {
+            Console.WriteLine("Equal  " + lengthOne.Equals(lengthTwo));
+        }
+    }
+}
