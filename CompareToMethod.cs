a832b14f417fb6cfe84d6a399014b68ba84e65af
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-08 08:47:55 +0530

    [sailesh] Add: Compare to method

---
+++ CompareToMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Line_Comparison
+{
+    class CompareToMethod
+    {
+        public double lengthOne, lengthTwo;
+
+        public CompareToMethod(double lengthOne, double lengthTwo)
+        {
+            this.lengthOne = lengthOne;
+            this.lengthTwo = lengthTwo;
+        }
+        public void FuncCompareToMethod()
+        {
+            Console.WriteLine("Compariso Result "+lengthOne.CompareTo(lengthTwo));
+        }
+    }
+}
